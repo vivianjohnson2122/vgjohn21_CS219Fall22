af099cfab311a053defcba7cef7b181dbb8d32d4
vivianjohnson <vivianjohnson2122@gmail.com>
2022-08-25 15:47:45 -0400

    My first Java program.

---
+++ src/HelloWorld.java
@@ -0,0 +1,6 @@
+// By convention, class names are capitalized and variables are camel case
+public class HelloWorld {
+    public static void main(String [] args) {
+        System.out.println("Hello World");
+    }
+}
