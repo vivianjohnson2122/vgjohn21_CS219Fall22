7ebd78e6c185ac77b4993dd3329f7de4590ec4d4
vivianjohnson <vivianjohnson2122@gmail.com>
2022-08-30 14:58:24 -0400

    todays changes.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,5 +2,9 @@
 public class HelloWorld {
     public static void main(String [] args) {
         System.out.println("Hello World");
+
+        int num = 8;
+        double pi = 3.14159;
+        boolean isFair = true;
     }
 }
