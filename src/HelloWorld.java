71eceb413f5866d917ef83bee8902e027dd8da62
vivianjohnson <vivianjohnson2122@gmail.com>
2022-08-30 15:14:25 -0400

    Final hello world

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,12 +1,21 @@
 // By convention, class names are capitalized and variables are camel case
 public class HelloWorld {
+
+    /*
+        this is a multiline block comment.
+        another comment line.
+     */
     public static void main(String [] args) {
+
         System.out.println("Hello World");
 
+        // Declaring variables:
         int num = 8;
         double pi = 3.14159;
         boolean isFair = true;
-        String name = "name";
-
+        String name = "CS219 Techniques of Computer Science";
+        // Scientific Notation:
+        double avagadros = 6.022140857e23;
+        double electronMass = 9.1093837015e-31;
     }
 }
